4368b3116167761b1f2ed908abddd1a3aa1d7376
crashdev226 <midashand44@gmail.com>
2020-01-25 11:30:42 +0200

    meetup item added

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,42 @@
 <script>
     import Header from './Shared/Header.svelte';
+    import MeetupItem from './Meetups/MeetupItem.svelte';
+
+    const meetups = [
+        {
+            id: "m1",
+            title: "Coding Bootcamp",
+            subtitle: "Learn to code in 2 hours",
+            description:
+                "In this meetup, we will have some experts that teach you how to code!",
+            imageUrl:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
+            address: "27th Nerd Road, 32523 New York",
+            contactEmail: "code@test.com"
+        },
+        {
+            id: "m2",
+            title: "Swim Together",
+            subtitle: "Let's go for some swimming",
+            description: "We will simply swim some rounds!",
+            imageUrl:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
+            address: "27th Nerd Road, 32523 New York",
+            contactEmail: "swim@test.com"
+        }
+    ];
 </script>
 
+<style>
+    #meetups {
+        margin-top: 5rem;
+    }
+</style>
+
 <Header />
+
+<section id="meetups">
+    {#each meetups as meetup}
+        <MeetupItem />
+    {/each}
+</section>
